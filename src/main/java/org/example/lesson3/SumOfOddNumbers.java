e1b54b38a1a8a0758d440f6bb1c40b1643dd958a
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-05-17 22:07:19 +0300

    Update SumOfOddNumbers.java

--- src/main/java/org/example/lesson3/SumOfOddNumbers.java
+++ src/main/java/org/example/lesson3/SumOfOddNumbers.java
@@ -3,6 +3,7 @@ package org.example.lesson3;
 Task 6
 Напишите программу, определяющую сумму всех нечетных чисел от 1 до 99
  */
+//+
 public class SumOfOddNumbers {
     public static void main(String[] args) {
         int sum = 0;

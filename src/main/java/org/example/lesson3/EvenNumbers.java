4ba1489e1ab3656018d478fcf6b3af122baa2495
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-05-17 22:05:04 +0300

    Update EvenNumbers.java

--- src/main/java/org/example/lesson3/EvenNumbers.java
+++ src/main/java/org/example/lesson3/EvenNumbers.java
@@ -3,6 +3,7 @@ package org.example.lesson3;
 Task 5
 Напишите программу вывода всех четных чисел от 2 до 100 включительно
  */
+//+
 public class EvenNumbers {
     public static void main(String[] args) {
         for (int i = 2; i <= 100; i += 2) {

38b8737b3284458bb08263ec638d30d64ad5602b
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-05-18 18:48:38 +0300

    Update Inches.java

--- src/main/java/org/example/lesson3/Inches.java
+++ src/main/java/org/example/lesson3/Inches.java
@@ -4,7 +4,7 @@ Task 4
 Напишите программу печати таблицы перевода расстояний из дюймов в сантиметры
 для значений длин от 1 до 20 дюймов. 1 дюйм = 2,54 см
  */
-
+//+
 public class Inches {
     public static void main(String[] args) {
         double myDouble = 2.54;

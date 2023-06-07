9fc0ed1de821f45e508e4104e1bdbd7bb93629c8
AnyaKulesh <annakuleshV94@gmail.com>
2023-06-08 00:13:20 +0300

    lesson8

--- src/main/java/org/example/homework/lesson8/Polyndrom.java
+++ src/main/java/org/example/homework/lesson8/Polyndrom.java
@@ -1,8 +1,12 @@
 package org.example.homework.lesson8;
 
-import java.util.Locale;
 import java.util.Scanner;
-
+/*
+Task 2
+Написать программу, которая определяет, является ли слово палиндромом.
+(Палиндром - это слово, которое читается одинаково слева направо и справа налево. Примеры: "топот", "шалаш", "А роза упала на лапу Азора".)
+Для работы со строками вам пригодится это - https://metanit.com/java/tutorial/7.1.php
+ */
 public class Polyndrom {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);

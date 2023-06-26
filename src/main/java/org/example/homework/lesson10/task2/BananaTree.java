dbf4119922e144a196cf2d39cf6ffdfa01084e89
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-06-26 11:16:18 +0300

    Update BananaTree.java

--- src/main/java/org/example/homework/lesson10/task2/BananaTree.java
+++ src/main/java/org/example/homework/lesson10/task2/BananaTree.java
@@ -1,5 +1,5 @@
 package org.example.homework.lesson10.task2;
-
+//банана, коконат - просто Enum достаточно было сделать
 public class BananaTree extends Tree {
     public BananaTree(Branch trunk) {
         super(Fruits.BANANA, trunk);

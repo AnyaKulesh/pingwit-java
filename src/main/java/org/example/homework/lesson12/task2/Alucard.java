e5fbd277e00f50592e50d9122917f57d4d1756e4
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-06-26 11:19:41 +0300

    Update Alucard.java

--- src/main/java/org/example/homework/lesson12/task2/Alucard.java
+++ src/main/java/org/example/homework/lesson12/task2/Alucard.java
@@ -3,6 +3,7 @@ package org.example.homework.lesson12.task2;
 /*
 Есть слово “alucard”. Нужно изменить порядок букв на обратный.
  */
+//+
 public class Alucard {
 
     private static final String WORD = "alucard";

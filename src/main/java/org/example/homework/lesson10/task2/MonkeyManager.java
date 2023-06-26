e16fc0980146d9d4b7caf88f93e50e2e9ce99c77
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-06-26 11:18:12 +0300

    Update MonkeyManager.java

--- src/main/java/org/example/homework/lesson10/task2/MonkeyManager.java
+++ src/main/java/org/example/homework/lesson10/task2/MonkeyManager.java
@@ -1,6 +1,7 @@
 package org.example.homework.lesson10.task2;
 
 public class MonkeyManager {
+    //еще можно метод с несколькими деревьями для 1-й обезьяны
     public void countFruitsByMonkey(Monkey monkey, Tree tree) {
         Integer numFruits = monkey.countFruits(tree);
         System.out.println(monkey.getName() + " : " + numFruits + " on " + tree.getType() + " tree");
